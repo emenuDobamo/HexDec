c3c0023759243503ca5e3acae247fac0671e6c6d
Emenu Dobamo <birhanu.emenu@gmail.com>
2016-09-11 22:33:39 -0600

    intial commit

---
+++ app/src/main/java/com/vnatures/emenu/vending/hexdec/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vnatures.emenu.vending.hexdec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
